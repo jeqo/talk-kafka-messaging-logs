f82a530c24b63e94ec214f1daacd6317e07adc15
Jorge Quilcate <quilcate.jorge@gmail.com>
2017-02-16 20:06:42 +0100

    clean monitoring

--- monitoring/start-jmx-scraper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-java -Dcom.sun.management.jmxremote.ssl=false \
-     -Djava.rmi.server.hostname=localhost \
-     -Dcom.sun.management.jmxremote.authenticate=false \
-     -Dcom.sun.management.jmxremote.port=5555 \
-     -jar jmx_prometheus_httpserver.jar 8888 kafka.yml
