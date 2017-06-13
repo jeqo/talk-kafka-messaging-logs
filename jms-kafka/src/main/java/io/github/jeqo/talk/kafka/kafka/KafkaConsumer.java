adbc331b490f4dcc5b3bcac7fa6a6b029f480f7f
Jorge Quilcate Otoya <quilcate.jorge@gmail.com>
2017-06-13 13:26:11 +0200

    add kafka consumer

---
+++ jms-kafka/src/main/java/io/github/jeqo/talk/kafka/kafka/KafkaConsumer.java
@@ -0,0 +1,58 @@
+package io.github.jeqo.talk.kafka.kafka;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Message;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.kafka.KafkaConstants;
+import org.apache.camel.impl.DefaultCamelContext;
+
+/**
+ *
+ */
+public class KafkaConsumer {
+
+    public static void main(String[] args) throws Exception {
+        try {
+            final CamelContext context = new DefaultCamelContext();
+            context.addRoutes(
+                    new RouteBuilder() {
+                        @Override
+                        public void configure() throws Exception {
+                            from("kafka:topic1" +
+                                    "?brokers=docker-vm:9092" +
+                                    "&groupId=group1" +
+                                    "&autoOffsetReset=earliest")
+                                    .process(exchange -> {
+                                        String messageKey = "";
+                                        if (exchange.getIn() != null) {
+                                            final Message message = exchange.getIn();
+                                            final Integer partitionId = (Integer) message
+                                                    .getHeader(KafkaConstants.PARTITION);
+                                            final String topicName = (String) message
+                                                    .getHeader(KafkaConstants.TOPIC);
+                                            if (message.getHeader(KafkaConstants.KEY) != null)
+                                                messageKey = (String) message
+                                                        .getHeader(KafkaConstants.KEY);
+                                            Object data = message.getBody();
+
+                                            System.out.println("topicName :: "
+                                                    + topicName + " partitionId :: "
+                                                    + partitionId + " messageKey :: "
+                                                    + messageKey + " message :: "
+                                                    + data + "\n");
+                                        }
+                                    }).to("log:input?level=TRACE");
+                        }
+                    });
+            context.setTracing(true);
+            context.start();
+
+            // let it run for 5 minutes before shutting down
+            Thread.sleep(5 * 60 * 1000);
+
+            context.stop();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
